127fa4492f8131fda4eaf533afb540b70bc1e6ea
Michael Horowitz <mjhorowitz714@yahoo.com>
2020-06-15 14:17:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   name_a = name_hash.to_a
 
   d_value= 1000
-  d_key= 0
+  d_key= nil
 
 
   name_a.each do |i|
@@ -13,9 +13,6 @@ def key_for_min_value(name_hash)
         d_value = i[1]
         d_key = i[0]
       end
-
    end
-
-  return d_key
-
+   d_key
 end  
